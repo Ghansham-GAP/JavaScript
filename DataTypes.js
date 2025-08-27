77127843a5def739dcb02fd3c623413c0f6c301f
Ghansham Pawar <ghansham.pawar.official@gmail.com>
2025-08-27 15:51:02 +0530

    DataTypes in JS

---
+++ DataTypes.js
@@ -0,0 +1,9 @@
+const arr = ["Gulabjamun"," Aamras"," Kheer"];
+console.log("My favourite desserts are : "+arr);
+
+let food = {
+    mainCourse : "Biryani",
+    sideDish : "Raita",
+    Dessert : "Gulabjamun"
+};
+console.log("My food preferences are : "+food.mainCourse,food.sideDish,food.Dessert);
