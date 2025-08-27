0d75c87ee06f7ab93d05cb6bb0aed9f88a24cdbc
Ghansham Pawar <ghansham.pawar.official@gmail.com>
2025-08-27 16:05:39 +0530

    Variables in JS

--- Variables.js
+++ Variables.js
@@ -1,5 +1,6 @@
 const name = "Ghansham Ashok Pawar";
 let age = 20;
+const caste = "ST";
 let isStudent = true;
 
-console.log("Name : "+name+ "\nAge : "+age+"\nIs the person a student? : "+isStudent);  
+console.log("Name : "+name+ "\nAge : "+age+"\nCaste : "+caste+"\nIs the person a student? : "+isStudent);  
